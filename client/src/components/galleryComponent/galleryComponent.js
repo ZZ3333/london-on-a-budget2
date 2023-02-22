3cb07086c33fac2b61202efe4e226c809cf8accb
Zahar <zahar.zelenskiy@gmail.com>
2023-02-22 16:39:17 +0000

    create gallery html component

---
+++ client/src/components/galleryComponent/galleryComponent.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const galleryComponent = () => {
+  return (
+    <div>
+      <h2></h2>
+      <p></p>
+      <img></img>
+    </div>
+  );
+};
+
+export default galleryComponent;
