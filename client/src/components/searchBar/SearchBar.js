a0400fd4bed05e88e283ce9191c75cc2149c5595
Alicia Moses <aliciamoses.dev@gmail.com>
2023-02-22 22:21:53 +0000

    Components now rendering in browser

--- client/src/components/searchBar/SearchBar.js
+++ client/src/components/searchBar/SearchBar.js
@@ -1,29 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-// const axios = require("axios")
-// import "./SearchBar.css";
 
-const SearchBar = () => {
-  const [query, setQuery] = useState("");
-  const handleChange = (event) => {
+const SearchBar = () =>{
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) =>{
     setQuery(event.target.value);
   };
 
-  const handleSubmit = async (event) => {
-    // event.preventDefault();
-    // try {
-    //   const response = await axios.get("url");
-    //   console.log(response.data);
-    // } catch (error) {
-    //   console.error(error);
-    // }
+  const handleSubmit = (event) =>{
+    event.preventDefault();
   };
   return (
-    <div>
-      <input type="text" value={query} onChange={handleChange}>
-        <button onClick={handleSubmit}>Search</button>
-      </input>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={query} onChange={handleInputChange} />
+      <button type="submit">Search</button>
+    </form>
   );
 };
 
--- server/app.js
+++ server/app.js
@@ -3,4 +3,20 @@ const express = require("express");
 const router = express.Router();
 const app = express;
 
+apiKey = "GDGXL4VWB5OHBTEDBQTN";
 
+const params = {
+  q: "javascript",
+  "location.address": "London",
+  price: "",
+  token: apiKey,
+};
+
+axios
+  .get("https://www.eventbriteapi.com/v3/organizers/55836839893/", { params })
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
