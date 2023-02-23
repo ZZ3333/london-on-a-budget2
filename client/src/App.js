bcfe56a2490f6b66749dc316e208da5d8afc9e0e
Caroline-Amanquah <carolineamanquah@gmail.com>
2023-02-23 17:14:28 +0000

    login and signup forms added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,14 @@ import SearchBar from './components/searchBar/SearchBar';
 import EventsGallery from './components/eventsGallery/EventsGallery';
 import IconsBar from './components/iconsBar/IconsBar';
 import Navbar from './components/navbar/Navbar';
+// import LoginForm from '../auth/LoginForm';
+import SignUpForm from './components/user/SignUpForm';
+// import {
+//   useNavigate,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+
 
 function App() {
   return (
@@ -24,5 +32,20 @@ function App() {
     </html>
   );
 }
+
+// const App = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Feed navigate={useNavigate()} />} />
+//       <Route path="/login" element={<LoginForm navigate={useNavigate()} />} />
+//       <Route
+//         path="/signup"
+//         element={<SignUpForm navigate={useNavigate()} />}
+//       />
+//       {/* <Route path="/" element={<Navbar navigate={useNavigate()} />} />
+//       <Route path="/user/:id" element={<Profile navigate={useNavigate()} />} /> */}
+//     </Routes>
+//   );
+// }
 
 export default App;
