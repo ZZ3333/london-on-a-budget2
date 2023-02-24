ba8ed0963ea83da8eb40a0b4d365c352d1ad2e85
Zahar <zahar.zelenskiy@gmail.com>
2023-02-24 13:11:03 +0000

    test users find controlller method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -10,7 +10,22 @@ const UsersController = {
         res.status(201).json({ message: "User created successfully" });
       }
     });
+  },
+  
+  Find: async (req, res) => {
+    const userId = req.params.id;
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: `User not found` });
+      }
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error finding user" });
+    }
   }
+
 };
 
 module.exports = UsersController;
