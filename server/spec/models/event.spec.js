064e83f94a716a133eced16d537e634c7dc9ac07
Alicia Moses <aliciamoses.dev@gmail.com>
2023-02-24 14:01:24 +0000

    Passing test for event name in event model spec

--- server/spec/models/event.spec.js
+++ server/spec/models/event.spec.js
@@ -12,9 +12,15 @@ describe("Event model", () => {
 
   it("stores an event object", () => {
     const event = new Event({
-
     });
     expect(event).toBeDefined();
+  });
+  it("has a name", () => {
+    const event = new Event({
+      name: "testEvent",
+    });
+    expect(event.name).toBeDefined();
+    expect(event.name).toEqual("testEvent");
   });
 
 });
