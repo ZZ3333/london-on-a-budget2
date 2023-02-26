c44b7bbdc18216c19a867300c61a4c2ee2441d3c
your name <your email>
2023-02-26 17:47:48 +0000

    rendered names tests for the signupform.js

--- client/src/components/__tests__/signupForm.test.js
+++ client/src/components/__tests__/signupForm.test.js
@@ -6,8 +6,21 @@ test ('should render Signup email component', () => {
     const SignUpElement = screen.getByTestId('email');
     expect(SignUpElement).toBeInTheDocument();
 })
+
 test ('should render Signup password component', () => {
     render(<SignUpForm/>);
     const SignUpElement = screen.getByTestId('password');
+    expect(SignUpElement).toBeInTheDocument();
+})
+
+test ('should render Signup firstName component', () => {
+    render(<SignUpForm/>);
+    const SignUpElement = screen.getByTestId('firstName');
+    expect(SignUpElement).toBeInTheDocument();
+})
+
+test ('should render Signup lastName component', () => {
+    render(<SignUpForm/>);
+    const SignUpElement = screen.getByTestId('lastName');
     expect(SignUpElement).toBeInTheDocument();
 })
