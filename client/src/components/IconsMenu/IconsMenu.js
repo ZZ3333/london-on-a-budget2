135c0b4e27ce3eafd712375f287f14592eb993ff
your name <your email>
2023-02-27 12:27:42 +0000

    added new tests for the icons menu

--- client/src/components/IconsMenu/IconsMenu.js
+++ client/src/components/IconsMenu/IconsMenu.js
@@ -7,7 +7,7 @@ const IconsMenu = () => {
         <button type="submit">
           <span data-testid="music"></span>
           <i class="bi bi-music-note-beamed"></i>
-        </button>        
+        </button>
       </a>
       <a href="/sports">
         <button type="sports">
@@ -18,17 +18,23 @@ const IconsMenu = () => {
       </a>
       <a href="/food-and-drink">
         <button type="submit">
+        <span data-testid="food_&_drinks">
           <i class="bi bi-cup-hot"></i>
+        </span>
         </button>
       </a>
       <a href="/visual-arts">
         <button type="submit">
+        <span data-testid="visual_arts">
           <i class="bi bi-brush"></i>
+        </span>
         </button>
       </a>
       <a href="/family-and-attractions">
         <button type="submit">
-          <i class="bi bi-emoji-smile-fill"></i>
+          <span data-testid="family_&_attractions">
+            <i class="bi bi-emoji-smile-fill"></i>
+          </span>
         </button>
       </a>
     </div>
