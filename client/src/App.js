5c113942af9f011bd7ff8f11b9d6c3334e0dbdfd
Ehijie <eaghedo1@gmail.com>
2023-02-28 01:32:13 +0000

    bBrowserRouter now functioning

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,47 @@
-import React from 'react';
-import './App.css';
-import './index.css';
-import Search from './components/Search/Search';
-import EventsView from './components/EventsView/EventsView';
-import IconsMenu from './components/IconsMenu/IconsMenu';
-import Navbar from './components/navbar/Navbar';
-// import LoginForm from '../auth/LoginForm';
-import SignUpForm from './components/user/SignUpForm';
-import LogInForm from './components/auth/LoginForm';
+import React from "react";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
-  return (
-    <html>
-      <head>
-        <title>Learn react</title>
-      </head>
-      <body>
-        <div className="landing-page">
-          <h1>London on a Budget</h1>
-          <Search />
-          <IconsMenu />
-          <EventsView />
-          <SignUpForm />
-          <LogInForm />
-          
-        </div>
-      </body>
-    </html>
-  );
-}
 
+	return (
+		<>
+			<BrowserRouter>
+				<div style={{
+					display: "flex",
+					background: 'black',
+					padding: '5px 0 5px 5px',
+					fontSize: '20px'
+				}}>
+					<div style={{ margin: '10px' }}>
+						<NavLink to="/" style={({ isActive }) => ({
+							color: isActive ? 'greenyellow' : 'white' })}>
+							Home
+						</NavLink>
+					</div>
+					<div style={{ margin: '10px' }}>
+						<NavLink to="/about" style={({ isActive }) => ({
+							color: isActive ? 'greenyellow' : 'white' })}>
+							About
+						</NavLink>
+					</div>
+					<div style={{ margin: '10px' }}>
+						<NavLink to="/contact" style={({ isActive }) => ({
+							color: isActive ? 'greenyellow' : 'white' })}>
+							Contact
+						</NavLink>
+					</div>
+				</div>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/about" element={<About />} />
+					<Route exact path="/contact" element={<Contact />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
 
 export default App;
