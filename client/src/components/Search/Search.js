f11844fc6e081a4d9ec73be84e3e161b06d6f199
Caroline-Amanquah <carolineamanquah@gmail.com>
2023-03-01 16:52:20 +0000

    updated searchbar

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './Search.css';
 
+
+
 const Search = () => {
   return (
         <div className="search-bar" data-testid="search">
@@ -23,13 +25,21 @@ const Search = () => {
           </div>
           <div className="search-column">
             <label htmlFor="price">Price:</label>
-            <input type="number" data-testid="price" className="price" />
-          </div>
+            {/* <input type="number" data-testid="price" className="price" /> */}
+            {/* <input data-testid="pi_input" type="range" min="0" max= "1000" step="0.25" />
+            <p>Value: <output id="value"></output></p>
+          </div> */}
+            <select data-testid="price" >
+            <option value="0-20">£0 - £20</option>
+            <option value="20-40">£20 - £40</option>
+            <option value="40-60">£40 - £60</option>
+            <option value="60">£60+</option>
+          </select>
           <button type="submit" data-testid="search-btn"className="search-btn">Search</button>
         </div>
+      </div>
   );
 };
 
 export default Search;
 
-{/* <input role='submit-button' data-testid='submit' type="submit" value="Submit" /> */}
