8d402083a96f647d817bf51f23c12c0e996d3f7a
Alicia Moses <aliciamoses.dev@gmail.com>
2023-03-01 21:00:56 +0000

    Imported Bootstrap CSS through CDN in index.html Removed Bootstrap CSS from in index.css Removed server.js

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,31 +1,5 @@
-// navbar on homepage : login and sign up button 
-
 import React from "react";
 import './Navbar.css';
-
-// const Navbar = () => {
-    // const [searchTerm, setSearchTerm] = useState('');
-    // const [searchResults, setSearchResults] = useState([]);
-
-    // const handleSignOut = () => {
-    //   window.localStorage.removeItem("token");
-    //   window.location.href = "/login";
-    // };
-//       return (
-//       <div align="right">
-//           <a href="/login-page">
-//           <button type="submit">
-//             <i class="login">Login</i>
-//           </button>
-//         </a>
-//         <a href="/signup-page">
-//           <button type="submit">
-//             <i class="signup">Signup</i>
-//           </button>
-//         </a>
-//       </div>
-//     );
-// };
 
 const Navbar = () => {
   return (
