b9fc048e00cab01fdb928fd3ba9dd37b200f626c
Caroline-Amanquah <carolineamanquah@gmail.com>
2023-03-01 01:20:31 +0000

    tested search elements: location, date, price and added option values to location, input data type of date added to Date enabling calendar display and number input type added to Price.

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 import './index.css';
 import Search from './components/Search/Search';
+import './components/Search/Search.css';
 import EventsView from './components/EventsView/EventsView';
 import IconsMenu from './components/IconsMenu/IconsMenu';
 import Navbar from './components/navbar/Navbar';
-// import LoginForm from '../auth/LoginForm';
 import SignUpForm from './components/user/SignUpForm';
 import LogInForm from './components/auth/LoginForm';
 
