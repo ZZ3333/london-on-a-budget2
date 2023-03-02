e16109c1d1b15630c671faf2f545e3e192818c9e
Alicia Moses <aliciamoses.dev@gmail.com>
2023-03-02 14:39:58 +0000

    Added Styling to LandingPage heroes

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -18,7 +18,7 @@ const LandingPage = () => {
               <span class="fs-4"></span>
             </a>
           </header>
-          <div class="p-5 mb-4 bg-light rounded-3">
+          <div class="p-5 mb-4 text-white bg-dark rounded-3">
             <div class="container-fluid py-5">
               <p class="col-md-8 fs-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -29,39 +29,55 @@ const LandingPage = () => {
                 nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-              <button class="btn btn-dark btn-lg" type="button">
+              <button class="btn btn-light btn-lg" type="button">
                 MAIN ACTION
               </button>
             </div>
           </div>
 
-          <div class="row align-items-md-stretch">
-            <div class="col-md-6">
-              <div class="h-100 p-5  bg-light rounded-3">
-                <h1 className="display-4">Experience</h1>
-                <p></p>
-                <button class="btn btn-outline-secondary" type="button">
-                  ACTION
-                </button>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="h-100 p-5 bg-light  rounded-3">
-                <h1 className="display-4">Experience</h1>
-                <p></p>
-                <button class="btn btn-outline-secondary" type="button">
-                  ACTION
-                </button>
-              </div>
-            </div>
-          </div>
-          <br></br>
+          <div class="row align-items-md-stretch"></div>
+      
           <div class="row align-items-md-stretch">
             <div class="col-md-12">
-              <div class="h-100 p-5 text-white bg-dark rounded-3">
-                <p>extra div here</p>
+              <div class="h-100 p-5  bg rounded-3">
+                <div class="container col-xxl-12 px-4 py-5 rounded-3">
+                  <div class="row flex-lg-row-reverse align-items-center g-5 py-5 shadow-lg">
+                    <div class="col-10 col-sm-8 col-lg-6 ">
+                      <img
+                        src="https://i.postimg.cc/zvQWtF2X/iphone-g657d0fa51-1920.jpg"
+                        class="d-block mx-lg-auto img-fluid"
+                        alt="Bootstrap Themes"
+                        width="700"
+                        height="500"
+                        loading="lazy"
+                      ></img>
+                    </div>
+                    <div class="col-lg-6">
+                      <h1 class="display-5 fw-bold lh-1 mb-3">TAGLINE</h1>
+                      <p class="lead">
+                        DESCRIBE USER EXPERIENCE WITH LEADS TO CALLS TO ACTION
+                      </p>
+                      <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                        <button
+                          type="button"
+                          class="btn btn-dark btn-lg px-4 me-md-2"
+                        >
+                          Join Us
+                        </button>
+                        <button
+                          type="button"
+                          class="btn btn-outline-secondary btn-lg px-4"
+                        >
+                          Log In
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+            
+           
           </div>
 
           <footer class="pt-3 mt-4 text-muted border-top"></footer>
--- client/src/components/user/SignUpForm.js
+++ client/src/components/user/SignUpForm.js
@@ -1,50 +1,35 @@
 import React, { useState } from "react";
-import './SignUpForm.css';
+import "./SignUpForm.css";
 
 const SignUpForm = ({ navigate }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [emailError, setEmailError] = useState(false);
-
-  const validateEmail = () => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
-
-
+// will add validation after
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (validateEmail()) {
-      setEmailError(false);
-      fetch("/users", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          firstName: firstName,
-          lastName: lastName,
-        }),
-      }).then((response) => {
-        if (response.status === 201) {
-          console.log("user created successfully");
-          navigate("/login");
-        } else {
-          console.log("user not created");
-          navigate("/signup");
-        }
-      })}
-      else {
-        setEmailError(true); 
+    fetch("/users", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+      }),
+    }).then((response) => {
+      if (response.status === 201) {
+        console.log("user created successfully");
+        navigate("/login");
+      } else {
+        console.log("user not created");
+        navigate("/signup");
       }
+    });
   };
 
   const handleEmailChange = (event) => {
@@ -63,55 +48,9 @@ const SignUpForm = ({ navigate }) => {
     setLastName(event.target.value);
   };
 
-
   return (
-    <div className="register">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input
-            placeholder="Email"
-            data-testid="email"
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="form-control"
-          />
-          <input
-            placeholder="Password"
-            data-testid="password"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="form-control"
-            required
-        minLength={8}
-        maxLength={20}
-      />
-          <input
-            placeholder="First Name"
-            data-testid="firstName"
-            type="text"
-            value={firstName}
-            onChange={handleFirstNameChange}
-            className="form-control"
-            required
-        minLength={2}
-      />
-          <input
-            placeholder="Last Name"
-            data-testid="lastName"
-            type="text"
-            value={lastName}
-            onChange={handleLastNameChange}
-            className="form-control"
-            required
-        minLength={2}
-      />
-          <button type="submit" class="btn btn-dark" value="Submit">
-            Submit
-          </button>
-        </div>
-      </form>
+    <div>
+      
     </div>
   );
 };
